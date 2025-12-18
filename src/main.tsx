a7c2092fe837a4f9ec044414a178e56d65e33a5a
Marana38 <maranagor10@gmail.com>
2025-12-18 12:23:39 +0200

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import "modern-normalize";
-
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import "modern-normalize/modern-normalize.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />{" "}
+  </React.StrictMode>
 );
