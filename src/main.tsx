d64d3d4941a67c11894d3be45e03a5ba4e5cbd07
Marana38 <maranagor10@gmail.com>
2025-12-19 11:10:58 +0200

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./components/App/App";
+import "./index.css";
+// import "modern-normalize";
 import "modern-normalize/modern-normalize.css";
 
-import "./index.css";
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />{" "}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
